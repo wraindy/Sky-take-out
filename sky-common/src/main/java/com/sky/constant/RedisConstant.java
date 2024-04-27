31395b355c2e2160a26ade7c04684cd19b73af5f
wraindy <wraindy9894@outlook.com>
2024-04-27 13:11:58 +0800

    feature(global): 配置Redis

---
+++ sky-common/src/main/java/com/sky/constant/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.sky.constant;
+
+/**
+ * @Author Wraindy
+ * @DateTime 2024/04/27 12:47
+ * Description
+ * Notice
+ **/
+
+public class RedisConstant {
+    public static final String SHOP_STATUS = "SHOP_STATUS";
+}
