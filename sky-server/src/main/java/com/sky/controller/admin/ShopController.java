17c1838570efd1f4d331aee3bf8a98af35405d0c
wraindy <wraindy9894@outlook.com>
2024-04-27 13:12:47 +0800

    feature(global): 新增获取和修改店铺营业状态的功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,47 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.RedisConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+/**
+ * @Author Wraindy
+ * @DateTime 2024/04/27 09:25
+ * Description
+ * Notice
+ **/
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+@Api(tags = "店铺操作接口(admin)")
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(RedisConstant.SHOP_STATUS);
+        if(status == null){
+            status = StatusConstant.DISABLE;
+        }
+        return Result.success(status);
+    }
+
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺营业状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置店铺营业状态：{}", Objects.equals(status, StatusConstant.ENABLE) ? "营业中..." : "打烊...");
+        redisTemplate.opsForValue().set(RedisConstant.SHOP_STATUS, status);
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,37 @@
+package com.sky.controller.user;
+
+import com.sky.constant.RedisConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * @Author Wraindy
+ * @DateTime 2024/04/27 12:53
+ * Description
+ * Notice
+ **/
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺操作接口(user)")
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(RedisConstant.SHOP_STATUS);
+        return Result.success(status);
+    }
+
+}
+
