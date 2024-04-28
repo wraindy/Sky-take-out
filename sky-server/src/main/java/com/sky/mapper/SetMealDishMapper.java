b5afb91a8825b3d124bcc1505f16fb88cf4fe87f
wraindy <wraindy9894@outlook.com>
2024-04-28 14:38:16 +0800

    style(SetMeal): 调整变量名

--- sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
@@ -19,11 +19,11 @@ public interface SetMealDishMapper {
      * 根据菜品id查询该菜品被关联的套餐个数
      * 在DishServiceImpl.deleteBatch方法的实现时，不建议把本方法放入for循环中查询，以此代替SetMealDishMapper.getSetMealIdsByDishIds方法
      * 因为网络延迟大于数据库内部查询延迟，应该尽可能减少访问数据库的频率
-     * @param id
+     * @param dishId
      * @return
      */
-    @Select("select count(*) from setmeal_dish where dish_id = #{id}")
-    int checkRelation(Long id);
+    @Select("select count(*) from setmeal_dish where dish_id = #{dishId}")
+    int checkRelation(Long dishId);
 
     /**
      * 根据菜品id，查询套餐id
