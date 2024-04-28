cfbca207f362129e2fd499593ecc545a328825b6
wraindy <wraindy9894@outlook.com>
2024-04-28 21:08:01 +0800

    feature(dish): 调整菜品的缓存管理中，redis的key

--- sky-common/src/main/java/com/sky/constant/RedisConstant.java
+++ sky-common/src/main/java/com/sky/constant/RedisConstant.java
@@ -13,8 +13,8 @@ public class RedisConstant {
     public static final String SHOP_STATUS = "SHOP_STATUS";
 
     // 根据分类id查询的菜品缓存
-    public static final String CATEGORY_CACHE = "CATEGORY:";
     // 分类包含菜品（Dish）和套餐（SetMeal）
     // 为区分套餐的缓存管理，菜品的在Redis中的key使用<DishCache::>
     // 而套餐管理的key使用<SetMealCache::>
+    public static final String CATEGORY_CACHE = "DishCache::";
 }
