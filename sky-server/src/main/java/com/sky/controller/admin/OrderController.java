aa01f66c6c2a0f1f3cb30ab1ebe33180c13fe4d7
wraindy <wraindy9894@outlook.com>
2024-05-06 00:25:01 +0800

    feature(admin/order): 新增查看订单详情

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -4,11 +4,13 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,12 @@ public class OrderController {
         log.info("动态查询条件ordersPageQueryDTO：{}",ordersPageQueryDTO);
         PageResult result = orderService.queryByCondition(ordersPageQueryDTO);
         return Result.success(result);
+    }
+
+    @GetMapping("/details/{id}")
+    @ApiOperation("根据订单表主键查询订单详情")
+    public Result<OrderVO> queryOrderDetail(@PathVariable Long id){
+        OrderVO orderVO = orderService.queryOrderDetail(id);
+        return Result.success(orderVO);
     }
 }
